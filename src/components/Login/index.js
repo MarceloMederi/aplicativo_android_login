9c52e71433d587f05eb0c7aecd28249b83517a51
sergiocouter <sergiocouter@gmail.com>
2023-07-28 16:44:50 -0300

    Adicionado a logo do projeto na tela  de login. Feito a estilização da tela de login e alterado o botão simples para um botão estilizavél

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,6 +1,7 @@
 // Importando as dependências necessárias do React e do React Native
 import React, { useState } from 'react';
-import { View, TextInput, Button, Alert } from 'react-native';
+import { View, TextInput, Button, Alert, StyleSheet, Image, Text, TouchableOpacity} from 'react-native';
+import Logo from "../../../assets/logo.png"
 
 // Definindo o componente Login como uma função. Ele recebe uma prop chamada onLoginSuccess.
 const Login = ({ onLoginSuccess }) => {
@@ -25,26 +26,35 @@ const Login = ({ onLoginSuccess }) => {
 
   // Retorna a estrutura do componente, a ser renderizada na tela.
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#E1F5FE' }}>
+    <View style={estilos.formulario}>
       {/* View que agrupa os campos de entrada de texto e o botão */}
+
+      <Image source={Logo} style={estilos.logo}></Image>
+      {/* Importação da imagem e estilização na constante 'estilos' */}
+
       <View>
         {/* Campo de entrada para o nome de usuário */}
         <TextInput
-          placeholder="Nome de usuário"
+          placeholder="Usuário"
+          placeholderTextColor={"#a2a29d"}
           value={username}
           onChangeText={text => setUsername(text)} // Quando o texto é alterado, atualiza o estado username.
-          style={{ marginBottom: 10, padding: 10, borderWidth: 1 }}
+          style={estilos.input}
         />
         {/* Campo de entrada para a senha */}
         <TextInput
           placeholder="Senha"
+          placeholderTextColor={"#a2a29d"}
           secureTextEntry // Define que o texto digitado será exibido de forma oculta.
           value={password}
           onChangeText={text => setPassword(text)} // Quando o texto é alterado, atualiza o estado password.
-          style={{ marginBottom: 10, padding: 10, borderWidth: 1 }}
+          style={estilos.input}
         />
         {/* Botão para realizar o login */}
-        <Button title="Entrar" onPress={handleLogin} />
+
+        <TouchableOpacity style={estilos.botao} onPress={handleLogin}>
+          <Text style={estilos.textoBotao}>Entrar</Text>
+        </TouchableOpacity>
       </View>
     </View>
   );
@@ -52,3 +62,40 @@ const Login = ({ onLoginSuccess }) => {
 
 // Exporta o componente Login para que possa ser utilizado em outros locais do código.
 export default Login;
+
+const estilos = StyleSheet.create({
+  formulario:{
+    backgroundColor: "#1A4888",
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  logo:{
+    width: 250,
+    height: 250,
+  },
+  input:{
+    marginBottom: 20,
+    paddingHorizontal: 100,
+    paddingVertical: 20,
+    borderWidth: 2,
+    borderColor: "#F7941E",
+    borderRadius: 10,
+    color: "#FFFFFF",
+    fontSize: 18,
+  },
+  botao:{
+    marginTop: 20,
+    backgroundColor: "#F7941E",
+    paddingVertical: 20,
+    borderRadius: 10,
+
+  },
+  textoBotao:{
+    textAlign: "center",
+    color: "#FFFFFF",
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  
+})
